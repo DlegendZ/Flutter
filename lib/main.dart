faafbb4747f6cf9d92cf74f2f6a70e3426888d99
DlegendZ <raynaldarvanlim@gmail.com>
2025-07-14 14:54:47 +0800

    statefulwidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,45 @@
-late String desc;
+import 'package:flutter/material.dart';
+
 void main() {
-  int age = 25;
-  double price = 24.55;
+  runApp(MyApp());
+}
 
-  String name = "Raynald";
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter Training')),
+        body: Center(child: counterWidget()),
+      ),
+    );
+  }
+}
 
-  // print("Hello, $name! This year I am ${age+1}");
-  // print("Hello, " + name + "!");
+class counterWidget extends StatefulWidget {
+  @override
+  counterWidgetState createState() => counterWidgetState();
+}
 
-  // print(age);
-  // print(price);
+class counterWidgetState extends State<counterWidget> {
+  int counter = 0;
 
-  String person_name = "Alice";
+  void increment() {
+    setState(() {
+      counter++;
+    });
+  }
 
-  bool isLoggedIn = true;
-
-  List<String> fruit = ["apple", "banana", "orange"];
-  List<int> number = [1, 2, 3];
-
-  // print(fruit[0]);
-  // print(number[1]);
-
-  Map<String, int> score = {
-    "Raynald" : 100,
-    "Nigel" : 87,
-  };
-
-  Set<int> num_set = {1,2,3,1};
-  // print(num_set);
-
-  var city = "Jakarta"; //String
-  var count = 10; //int
-
-  dynamic value = "Raynald";
-  value = 1;
-  value = "Raynald";
-
-  final country;
-  country = "Indonesia";
-  // print(country);
-
-  const furnit = "chair";
-  // print(furnit);
-
-  desc = "apple is sweet";
-  print(desc);
-
-  String ? pet_name = "Ivan";
-  pet_name ??= "Rich";
-
-  print(pet_name);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text('Counter : $counter', style: TextStyle(fontSize: 24)),
+        SizedBox(height: 12),
+        ElevatedButton(onPressed: increment, child: Text('Add')),
+      ],
+    );
+  }
 }
