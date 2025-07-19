43aa29e9fd2e0ed878831d3981a9b7c2e6a9992a
DlegendZ <raynaldarvanlim@gmail.com>
2025-07-19 18:01:25 +0800

    Session_4

---
+++ lib/CartModel.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+
+class CartModel extends ChangeNotifier {
+  final List<String> _items = [];
+
+  List<String> get items => _items;
+
+  void add(String item) {
+    _items.add(item);
+    notifyListeners(); // kasih tahu semua anak-anak yang dengerin
+  }
+}
