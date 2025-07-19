c10c69fb73209bb2148712adae760c26492f1427
DlegendZ <raynaldarvanlim@gmail.com>
2025-07-19 17:56:34 +0800

    session_4

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'CartModel.dart'; // import modelnya
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CartModel(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,37 +16,63 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(title: Text('Flutter Training')),
-        body: Center(child: counterWidget()),
+      title: 'Toko Mainan',
+      home: ToyCatalogPage(),
+    );
+  }
+}
+
+class ToyCatalogPage extends StatelessWidget {
+  final List<String> toyList = ['Robot', 'Boneka', 'Puzzle', 'Lego'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daftar Mainan'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => CartPage()),
+              );
+            },
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: toyList.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(toyList[index]),
+            trailing: ElevatedButton(
+              onPressed: () {
+                context.read<CartModel>().add(toyList[index]);
+              },
+              child: Text('Tambah'),
+            ),
+          );
+        },
       ),
     );
   }
 }
 
-class counterWidget extends StatefulWidget {
-  @override
-  counterWidgetState createState() => counterWidgetState();
-}
-
-class counterWidgetState extends State<counterWidget> {
-  int counter = 0;
-
-  void increment() {
-    setState(() {
-      counter++;
-    });
-  }
-
+class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Text('Counter : $counter', style: TextStyle(fontSize: 24)),
-        SizedBox(height: 12),
-        ElevatedButton(onPressed: increment, child: Text('Add')),
-      ],
+    var cart = context.watch<CartModel>();
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Keranjang')),
+      body: ListView.builder(
+        itemCount: cart.items.length,
+        itemBuilder: (context, index) => ListTile(
+          title: Text(cart.items[index]),
+        ),
+      ),
     );
   }
 }
